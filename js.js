7ebe31e3468dc093ac0359610e0016a16903a0ed
CT <tellezc007@gmail.com>
2022-03-31 21:41:06 -0700

    add base files and add variables for each aspect for password

---
+++ js.js
@@ -0,0 +1,33 @@
+// Assignment Code
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numChar = "123456789";
+var specialChar = "!@#$%^&*()_+=-{{}[]:;";
+var allChar = "";
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+}
+
+let pwdLength = prompt(
+    "How long would you like your password to be? between 8-128.");
+if (pwdLength < 8 || pwdLength > 128 || isNaN(parseInt(pwdLength))) {
+    alert("Please enter a number between 8-128.");
+} else {
+    let lowerCase = confirm("would you like to use lowercase characters?");
+    if (lowerCase) {
+        allChar += lowercaseChar
+    };
+    let upperCase = confirm("")
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
